c1e1d4cbac133dd82dc425a44d2b189ce13011ac
CachorroC <juankpato87@gmail.com>
2023-07-11 16:43:18 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
    8 Please enter the commit message for your changes. Lines starting
    8 with '8' will be ignored, and an empty message aborts the commit.
    8
    8 On branch main
    8 Your branch is up to date with 'origin/main'.
    8
    8 Changes to be committed:
    8	modified:   .eslintrc.js
    8	modified:   src/components/card/CarpetasCard/list.tsx
    8	modified:   src/lib/Carpetas/index.ts
    8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,29 @@
 module.exports = {
   extends: 'next/core-web-vitals',
   rules: {
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never']
+    quotes: [ 'error', 'single' ],
+    indent: [
+      'error',
+      2, {
+        'ArrayExpression': 'first',
+        'CallExpression': {
+          'arguments': 'first'
+        },
+        'FunctionDeclaration': {
+          'body': 2,
+          'parameters': 'first'
+        },
+        'FunctionExpression': {
+          'body': 2,
+          'parameters': 'first'
+        },
+        'ImportDeclaration': 'first',
+        'ObjectExpression': 'first',
+        'SwitchCase': 1,
+        'VariableDeclarator': 'first',
+        'offsetTernaryExpressions': true
+      }
+    ]
   }
-};
+}
