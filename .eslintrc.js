0090ca0c0146f6655c9dc818242474f240846883
CachorroC <juankpato87@gmail.com>
2023-07-11 16:47:38 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
    5 Please enter the commit message for your changes. Lines starting
    5 with '5' will be ignored, and an empty message aborts the commit.
    5
    5 On branch main
    5 Your branch is up to date with 'origin/main'.
    5
    5 Changes to be committed:
    5	modified:   .eslintrc.js
    5	modified:   public/offline.html
    5	modified:   src/components/card/ActuacionesCard/actuaciones.module.scss
    5	modified:   src/components/card/CarpetasCard/carpetas.module.scss
    5	modified:   src/components/card/CarpetasCard/list.tsx
    5	modified:   src/components/card/NotasCard/notas.module.scss
    5	modified:   src/components/card/ProcesosCard/procesos.module.scss
    5	modified:   src/components/card/card.module.scss
    5	modified:   src/components/form/form.module.scss
    5	modified:   src/components/navbar/navbar.module.scss
    5	modified:   src/components/nota/note.module.scss
    5	modified:   src/components/search/searchbar.module.scss
    5	modified:   src/lib/Carpetas/index.ts
    5	modified:   src/styles/scss/theme/variables.scss
    5

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,29 @@ module.exports = {
   extends: 'next/core-web-vitals',
   rules: {
     'linebreak-style': ['error', 'unix'],
-    quotes: [ 'error', 'single' ],
+    quotes: ['error', 'single'],
     indent: [
       'error',
-      2, {
-        'ArrayExpression': 'first',
-        'CallExpression': {
-          'arguments': 'first'
+      2,
+      {
+        ArrayExpression: 'first',
+        CallExpression: {
+          arguments: 'first'
         },
-        'FunctionDeclaration': {
-          'body': 2,
-          'parameters': 'first'
+        FunctionDeclaration: {
+          body: 2,
+          parameters: 'first'
         },
-        'FunctionExpression': {
-          'body': 2,
-          'parameters': 'first'
+        FunctionExpression: {
+          body: 2,
+          parameters: 'first'
         },
-        'ImportDeclaration': 'first',
-        'ObjectExpression': 'first',
-        'SwitchCase': 1,
-        'VariableDeclarator': 'first',
-        'offsetTernaryExpressions': true
+        ImportDeclaration: 'first',
+        ObjectExpression: 'first',
+        SwitchCase: 1,
+        VariableDeclarator: 'first',
+        offsetTernaryExpressions: true
       }
     ]
   }
-}
+};
--- src/components/card/CarpetasCard/list.tsx
+++ src/components/card/CarpetasCard/list.tsx
@@ -29,17 +29,17 @@ const Fecha = async ({
 }: {
   idProceso: number;
 }) => {
-    const acts = await getActuaciones(idProceso);
+  const acts = await getActuaciones(idProceso);
 
-    if (acts.length === 0) {
-      return null;
-    }
+  if (acts.length === 0) {
+    return null;
+  }
 
-    return (
-      <div className={styles.date}>
-        {fixFechas(acts[0].fechaActuacion)}
-      </div>
-    );
+  return (
+    <div className={styles.date}>
+      {fixFechas(acts[0].fechaActuacion)}
+    </div>
+  );
 };
 
 export const DemandaContainer = ({
@@ -47,71 +47,71 @@ export const DemandaContainer = ({
 }: {
   demanda: Demanda;
 }) => {
-    const {
-      Departamento,
-      Municipio,
-      VencimientoPagare,
-      EntregadeGarantiasAbogado,
-      Radicado,
-      CapitalAdeudado,
-      Proceso,
-      Ubicacion,
-      Juzgado,
-      Obligacion
-    } = demanda;
+  const {
+    Departamento,
+    Municipio,
+    VencimientoPagare,
+    EntregadeGarantiasAbogado,
+    Radicado,
+    CapitalAdeudado,
+    Proceso,
+    Ubicacion,
+    Juzgado,
+    Obligacion
+  } = demanda;
 
-    return (
-      <div className={styles.section}>
-        <h1 className={typography.headlineMedium}>
-          {Radicado}
-        </h1>
-        <h2
-          className={
-            typography.titleMedium
-          }>{`${Departamento}: ${Municipio}`}</h2>
-        {VencimientoPagare && (
-          <p className={typography.labelMedium}>
-            {fixFechas(VencimientoPagare)}
-          </p>
-        )}
-        {EntregadeGarantiasAbogado && (
-          <p className={typography.labelSmall}>
-            {fixFechas(EntregadeGarantiasAbogado)}
-          </p>
-        )}
-        {CapitalAdeudado && (
-          <p className={typography.labelSmall}>
-            {CapitalAdeudado}
-          </p>
-        )}
-      </div>
-    );
+  return (
+    <div className={styles.section}>
+      <h1 className={typography.headlineMedium}>
+        {Radicado}
+      </h1>
+      <h2
+        className={
+          typography.titleMedium
+        }>{`${Departamento}: ${Municipio}`}</h2>
+      {VencimientoPagare && (
+        <p className={typography.labelMedium}>
+          {fixFechas(VencimientoPagare)}
+        </p>
+      )}
+      {EntregadeGarantiasAbogado && (
+        <p className={typography.labelSmall}>
+          {fixFechas(EntregadeGarantiasAbogado)}
+        </p>
+      )}
+      {CapitalAdeudado && (
+        <p className={typography.labelSmall}>
+          {CapitalAdeudado}
+        </p>
+      )}
+    </div>
+  );
 };
 
 export async function ListCardCarpetasNFechas() {
-    const req = await fetch(
-      `${getBaseUrl()}/api/Carpetas`
-    );
+  const req = await fetch(
+    `${getBaseUrl()}/api/Carpetas`
+  );
 
-    const carpetas =
+  const carpetas =
     (await req.json()) as monCarpetaDemandado[];
 
-    return (
-      <>
-        {carpetas.map((carpeta, index, arr) => {
-            return (
-              <CarpetaCard
-                Carpeta={carpeta}
-                key={carpeta._id}>
-                <Suspense fallback={<Loader />}>
-                  <Fecha
-                    key={carpeta._id + 'fecha'}
-                    idProceso={carpeta.idProceso}
-                  />
-                </Suspense>
-              </CarpetaCard>
-            );
-        })}
-      </>
-    );
+  return (
+    <>
+      {carpetas.map((carpeta, index, arr) => {
+        return (
+          <CarpetaCard
+            Carpeta={carpeta}
+            key={carpeta._id}>
+            <Suspense fallback={<Loader />}>
+              <Fecha
+                key={carpeta._id + 'fecha'}
+                idProceso={carpeta.idProceso}
+              />
+            </Suspense>
+          </CarpetaCard>
+        );
+      })}
+    </>
+  );
 }
