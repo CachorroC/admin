6dc2993eb2faefab14b365a8900129727fb07257
CachorroC <juankpato87@gmail.com>
2023-08-17 23:25:55 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
    2 Please enter the commit message for your changes. Lines starting
    2 with '2' will be ignored, and an empty message aborts the commit.
    2
    2 On branch main
    2 Your branch is up to date with 'origin/main'.
    2
    2 Changes to be committed:
    2	modified:   src/components/Accordion/accordion.module.css.d.ts
    2	modified:   src/components/Buttons/buttons.module.css.d.ts
    2	modified:   src/components/card/ActuacionesCard/actuaciones.module.css.d.ts
    2	modified:   src/components/card/CarpetasCard/carpetas.module.css.d.ts
    2	modified:   src/components/card/NotasCard/notas.module.css.d.ts
    2	modified:   src/components/card/ProcesosCard/procesos.module.css.d.ts
    2	new file:   src/components/card/card.module.css.d.ts
    2	modified:   src/components/card/carpeta.module.css.d.ts
    2	modified:   src/components/form/form.module.css.d.ts
    2	modified:   src/components/modal/modal.module.css.d.ts
    2	modified:   src/components/navbar/navbar.module.css.d.ts
    2	modified:   src/components/nota/accordion.module.css.d.ts
    2	modified:   src/components/nota/note.module.css.d.ts
    2	modified:   src/components/search/searchbar.module.css.d.ts
    2	modified:   src/styles/box.module.css.d.ts
    2	modified:   src/styles/fonts/typeface.module.css.d.ts
    2	modified:   src/styles/fonts/typography.module.css.d.ts
    2	modified:   src/styles/globals.css.d.ts
    2	modified:   src/styles/layout.module.css.d.ts
    2	modified:   src/styles/theme/colors.module.css.d.ts
    2	modified:   src/styles/theme/css/colors.module.css.d.ts
    2	modified:   src/styles/variables.module.css.d.ts
    2

--- src/components/card/carpeta.module.css.d.ts
+++ src/components/card/carpeta.module.css.d.ts
@@ -1,9 +1,9 @@
 declare const styles: {
-  readonly 'card': string;
-  readonly 'cardIsActive': string;
-  readonly 'cardInner': string;
-  readonly 'cardFront': string;
-  readonly 'cardBack': string;
+  readonly "card": string;
+  readonly "cardIsActive": string;
+  readonly "cardInner": string;
+  readonly "cardFront": string;
+  readonly "cardBack": string;
 };
 export = styles;
 
--- src/components/nota/accordion.module.css.d.ts
+++ src/components/nota/accordion.module.css.d.ts
@@ -1,10 +1,10 @@
 declare const styles: {
-  readonly 'accordion': string;
-  readonly 'content': string;
-  readonly 'isDone': string;
-  readonly 'notDone': string;
-  readonly 'button': string;
-  readonly 'ButtonTextHelper': string;
+  readonly "accordion": string;
+  readonly "content": string;
+  readonly "isDone": string;
+  readonly "notDone": string;
+  readonly "button": string;
+  readonly "ButtonTextHelper": string;
 };
 export = styles;
 
