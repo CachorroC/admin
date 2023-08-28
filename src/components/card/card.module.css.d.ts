17ecc469ddd2850cff17bf329b90650497ac6048
CachorroC <juankpato87@gmail.com>
2023-08-27 22:24:07 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
    8 Please enter the commit message for your changes. Lines starting
    8 with '8' will be ignored, and an empty message aborts the commit.
    8
    8 On branch main
    8 Your branch is up to date with 'origin/main'.
    8
    8 Changes to be committed:
    8	modified:   .gitignore
    8	new file:   src/components/Accordion/accordion.module.css.d.ts
    8	new file:   src/components/Accordion/accordion.module.scss.d.ts
    8	new file:   src/components/Buttons/buttons.module.css.d.ts
    8	new file:   src/components/Buttons/buttons.module.scss.d.ts
    8	new file:   src/components/card/ActuacionesCard/actuaciones.module.css.d.ts
    8	new file:   src/components/card/ActuacionesCard/actuaciones.module.scss.d.ts
    8	new file:   src/components/card/CarpetasCard/carpetas.module.css.d.ts
    8	new file:   src/components/card/CarpetasCard/carpetas.module.scss.d.ts
    8	new file:   src/components/card/NotasCard/notas.module.css.d.ts
    8	new file:   src/components/card/NotasCard/notas.module.scss.d.ts
    8	new file:   src/components/card/ProcesosCard/procesos.module.css.d.ts
    8	new file:   src/components/card/ProcesosCard/procesos.module.scss.d.ts
    8	new file:   src/components/card/card.module.css.d.ts
    8	new file:   src/components/card/card.module.scss.d.ts
    8	new file:   src/components/form/form.module.css.d.ts
    8	new file:   src/components/form/form.module.scss.d.ts
    8	new file:   src/components/loading-dots.module.css
    8	new file:   src/components/modal/modal.module.css.d.ts
    8	new file:   src/components/modal/modal.module.scss.d.ts
    8	new file:   src/components/navbar/navbar.module.css.d.ts
    8	new file:   src/components/navbar/navbar.module.scss.d.ts
    8	new file:   src/components/nota/accordion.module.css.d.ts
    8	new file:   src/components/nota/accordion.module.scss.d.ts
    8	new file:   src/components/nota/note.module.css.d.ts
    8	new file:   src/components/nota/note.module.scss.d.ts
    8	new file:   src/components/search/searchbar.module.css.d.ts
    8	new file:   src/components/search/searchbar.module.scss.d.ts
    8	new file:   src/styles/box.module.css.d.ts
    8	new file:   src/styles/box.module.scss.d.ts
    8	new file:   src/styles/fonts/typeface.module.css.d.ts
    8	new file:   src/styles/fonts/typeface.module.scss.d.ts
    8	new file:   src/styles/fonts/typography.module.css.d.ts
    8	new file:   src/styles/fonts/typography.module.scss.d.ts
    8	new file:   src/styles/globals.css.d.ts
    8	new file:   src/styles/globals.scss.d.ts
    8	new file:   src/styles/layout.module.css.d.ts
    8	new file:   src/styles/layout.module.scss.d.ts
    8	new file:   src/styles/theme/colors.module.css.d.ts
    8	new file:   src/styles/theme/colors.module.scss.d.ts
    8	new file:   src/styles/theme/darkTokens.css.d.ts
    8	new file:   src/styles/theme/lightTokens.css.d.ts
    8	new file:   src/styles/theme/tokens.css.d.ts
    8	new file:   src/styles/theme/variables.css.d.ts
    8	new file:   src/styles/theme/variables.module.css.d.ts
    8	new file:   src/styles/theme/variables.module.scss.d.ts
    8	new file:   src/styles/variables.module.css.d.ts
    8	new file:   src/styles/variables.module.scss.d.ts
    8

---
+++ src/components/card/card.module.css.d.ts
@@ -0,0 +1,26 @@
+declare const styles: {
+  readonly 'container': string;
+  readonly 'date': string;
+  readonly 'actuacion': string;
+  readonly 'anotascion': string;
+  readonly 'card': string;
+  readonly 'isActive': string;
+  readonly 'notActive': string;
+  readonly 'icon': string;
+  readonly 'section': string;
+  readonly 'sub': string;
+  readonly 'title': string;
+  readonly 'content': string;
+  readonly 'links': string;
+  readonly 'divider': string;
+  readonly 'updated': string;
+  readonly 'anotacion': string;
+  readonly 'fecha': string;
+  readonly 'dummytxt': string;
+  readonly 'link': string;
+  readonly 'tooltiptext': string;
+  readonly 'error': string;
+  readonly 'linkIsActive': string;
+};
+export = styles;
+
---
+++ src/components/search/searchbar.module.scss.d.ts
@@ -0,0 +1,16 @@
+export type Styles = {
+  container: string;
+  date: string;
+  icon: string;
+  input: string;
+  inputForm: string;
+  isActive: string;
+  notActive: string;
+  sub: string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
